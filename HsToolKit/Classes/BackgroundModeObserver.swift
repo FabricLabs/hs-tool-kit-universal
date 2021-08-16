e4973fd0de4a6bade44dbb4feb9c3f3d1a60ff33
cuhte3 <sintezzlab@gmail.com>
2021-08-16 15:04:10 +0300

    guard UIKit code

--- HsToolKit/Classes/BackgroundModeObserver.swift
+++ HsToolKit/Classes/BackgroundModeObserver.swift
@@ -5,27 +5,35 @@ public class BackgroundModeObserver {
     public static let shared = BackgroundModeObserver()
 
     private let foregroundFromExpiredBackgroundSubject = PublishSubject<Void>()
+    #if os(iOS)
     private var backgroundTask: UIBackgroundTaskIdentifier = UIBackgroundTaskIdentifier.invalid
+    #endif
 
     init() {
+        #if os(iOS)
         NotificationCenter.default.addObserver(self, selector: #selector(appCameToBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(appCameToForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+        #endif
     }
 
     @objc private func appCameToBackground() {
+        #if os(iOS)
         backgroundTask = UIApplication.shared.beginBackgroundTask {
             UIApplication.shared.endBackgroundTask(self.backgroundTask)
             self.backgroundTask = UIBackgroundTaskIdentifier.invalid
         }
+        #endif
     }
 
     @objc private func appCameToForeground() {
+        #if os(iOS)
         if backgroundTask != UIBackgroundTaskIdentifier.invalid {
             UIApplication.shared.endBackgroundTask(backgroundTask)
             backgroundTask = UIBackgroundTaskIdentifier.invalid
         } else {
             foregroundFromExpiredBackgroundSubject.onNext(())
         }
+        #endif
     }
 
     public var foregroundFromExpiredBackgroundObservable: Observable<Void> {
